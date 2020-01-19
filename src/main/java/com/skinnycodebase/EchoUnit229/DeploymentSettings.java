617ba436ded5f3cd87c5aae4ef51f25e580515e3
Rigo <skinny229@gmail.com>
2020-01-19 03:42:36 -0500

    updated env settings

--- src/main/java/com/skinnycodebase/EchoUnit229/DeploymentSettings.java
+++ src/main/java/com/skinnycodebase/EchoUnit229/DeploymentSettings.java
@@ -11,17 +11,17 @@ public class DeploymentSettings {
 
 
 
-    public static final String BOT_AUTH_TOKEN = System.getenv("DISCORD_UNIT229_TOKEN");
+    public static final String BOT_AUTH_TOKEN = System.getenv("AUTH_TOKEN");
 
-    public static final String BOIS_ROLE_ID = System.getenv("BOIS_ROLE_ID");
+    public static final String BOIS_ROLE_ID = "645047793500815387";
 
-    public static final String LFGBOT_CHANNEL_ID = System.getenv("LFGBOT_ID");
+    public static final String LFGBOT_CHANNEL_ID = "661307549496115232";
 
-    public static final String BOT_ID = System.getenv("BOT_ID");
+    public static final String BOT_ID ="661304672832847872";
 
-    public static final String ESO_GUILD_ID = System.getenv("ESO_GUILD_ID");
+    public static final String ESO_GUILD_ID = "623999906646065172";
 
-    public static final String DEV_ID = System.getenv("DEV_ID");
+    public static final String DEV_ID = "142673915964030976";
 
     public static final String API_CONTROLLER_VERSION = "v1";
 
