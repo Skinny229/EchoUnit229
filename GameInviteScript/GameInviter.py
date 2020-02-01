57c3a0e60b2147c57be66b1d62717af7fab2da6e
Rigo <skinny229@gmal.com>
2020-02-01 13:54:28 -0500

    Readme progress

--- GameInviteScript/GameInviter.py
+++ GameInviteScript/GameInviter.py
@@ -13,7 +13,7 @@ def getPublicGameInvite():
 	
 	if data:##if not null
 		lobbyid = data['sessionid']
-		invite = "-creategame " + lobbyid
+		invite = "-creategame " + lobbyid + " "
 
 	clipboard.copy(invite)
 		
