c64778b61ba6d88092bcd34a324750586a9c5dc4
RoxTytan <62077444+RoxTytan@users.noreply.github.com>
2020-03-12 13:47:05 -0400

    Update Roll.java

--- src/main/java/com/skinnycodebase/EchoUnit229/discordintegration/commands/Roll.java
+++ src/main/java/com/skinnycodebase/EchoUnit229/discordintegration/commands/Roll.java
@@ -1,9 +1,25 @@
 package com.skinnycodebase.EchoUnit229.discordintegration.commands;
 
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import java.util.Random;
 
 public class Roll {
+    
+    public int Roller(int numOfDice, int numOfSides) {
+        Random rnd = new Random();
+        int total = 0;
+        for (i=0;i<numOfDice;i++){
+            int x = rnd.nextInt(numOfSides)+1;
+            total += x;
+        }
+        return total;
+    }
 
     public static void run(MessageReceivedEvent event) {
+        String rollCmd = event.getMessage().getContentRaw();
+        int numOfDice,numOfSides;
+        if(rollCmd.startswith("-")){
+            
+        }
     }
 }
